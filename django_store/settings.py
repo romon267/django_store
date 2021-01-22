9804009c0a735939e995655044432a68d158f665
unknown <romon267@gmail.com>
2021-01-22 17:25:46 +0300

    3rd try

--- django_store/settings.py
+++ django_store/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_DS')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-store-romon.herokuapp.com']
 
 
 # Application definition
@@ -157,3 +158,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('DS_AWS_BUCKET')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
